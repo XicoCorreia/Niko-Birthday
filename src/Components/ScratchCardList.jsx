f06f07013efec27bb8b2ff9aa120a6b80ccc265b
XicoCorreia <xicocorreia17@gmail.com>
2025-07-10 19:43:42 +0200

    update to obj

--- src/Components/ScratchCardList.jsx
+++ src/Components/ScratchCardList.jsx
@@ -3,12 +3,34 @@ import ScratchCard from './ScratchCard';
 import './ScratchCardList.css';
 
 const ScratchCardList = () => {
+  const cardData = {
+    day1: {
+      day:"1",
+      theme:"1", 
+      hint:"1"
+    },
+    day2: {
+      day:"2",
+      theme:"2", 
+      hint:"2"
+    },
+    day3: {
+      day:"3",
+      theme:"3", 
+      hint:"3"
+    },
+    day4: {
+      day:"4",
+      theme:"4", 
+      hint:"4"
+    }
+  }
   return (
     <div className="scratch-card-list">
-      <ScratchCard day="1" theme="1" hint="1" />
-      <ScratchCard day="2" theme="2" hint="2" />
-      <ScratchCard day="3" theme="3" hint="3" />
-      <ScratchCard day="4" theme="4" hint="4" />
+      <ScratchCard {...cardData.day1} />
+      <ScratchCard {...cardData.day2} />
+      <ScratchCard {...cardData.day3} />
+      <ScratchCard {...cardData.day4} />
     </div>
   );
 };
